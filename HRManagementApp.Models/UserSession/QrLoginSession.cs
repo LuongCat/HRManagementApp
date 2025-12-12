95d5d4942919b2723ed0e73c515cd445aac1c0d1
SLMR05-05 <jdurisk@gmail.com>
2025-12-12 22:06:34 +0700

    ko co j

---
+++ HRManagementApp.Models/UserSession/QrLoginSession.cs
@@ -0,0 +1,11 @@
+﻿// File: QrLoginSession.cs
+namespace HRManagementApp.Models
+{
+    public class QrLoginSession
+    {
+        public string SessionId { get; set; }
+        public bool IsConfirmed { get; set; }
+        public string Username { get; set; } // Nếu muốn biết ai đăng nhập
+        public DateTime CreatedTime { get; set; }
+    }
+}
